c93db660600d97db43cc8e71f570169293dd5888
Frisk <2317216814@qq.com>
2026-06-15 19:00:48 +0800

    feat: 新增头像组件

---
+++ src/components/Avatar-component.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="Avatar-component">
+    </div>
+</template>
+
+<style scoped>
+.Avatar-component{
+    background: url(/public/images/my.jpg) no-repeat center center;
+    width: 200px;
+    height: 200px;
+    border-radius: 20px;
+    background-size: cover;
+}
+</style>
